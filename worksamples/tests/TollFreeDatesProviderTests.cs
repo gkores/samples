f01b32d2a2f9428921146f3ba8ad8f3c719c7255
Gustav Kores <gustav@oneseventy.se>
2025-04-17 21:10:52 +0200

    chore: Add additional tests
    Cover the toll-free dates of 2013.

--- worksamples/tests/TollFreeDatesProviderTests.cs
+++ worksamples/tests/TollFreeDatesProviderTests.cs
@@ -48,4 +48,36 @@ public class TollFreeDatesProviderTests
         //Assert
         Assert.False(actual);
     }
+    
+    [Theory]
+    [InlineData(2013,1,1)]
+    [InlineData(2013,3,28)]
+    [InlineData(2013,3,29)]
+    [InlineData(2013,4,1)]
+    [InlineData(2013,4,30)]
+    [InlineData(2013,5,1)]
+    [InlineData(2013,5,8)]
+    [InlineData(2013,5,9)]
+    [InlineData(2013,6,5)]
+    [InlineData(2013,6,6)]
+    [InlineData(2013,6,21)]
+    [InlineData(2013,7,5)]
+    [InlineData(2013,7,20)]
+    [InlineData(2013,11,1)]
+    [InlineData(2013,12,24)]
+    [InlineData(2013,12,25)]
+    [InlineData(2013,12,26)]
+    [InlineData(2013,12,31)]
+    public void GivenATollFreeDate_WhenEvaluated_ThenProviderShouldReturnTrue(int year, int month, int day)
+    {
+        //Arrange
+        var sut = new TollFreeDateProvider();
+        var knownTollFreeDate = new DateTime(year, month, day);
+
+        //Act
+        var actual = sut.IsTollFreeDate(knownTollFreeDate);
+
+        //Assert
+        Assert.True(actual);
+    }
 }
