82b5fd76f26385fcf6bf5245e94f9ba34187d154
Gustav Kores <gustav@oneseventy.se>
2025-04-21 11:55:55 +0200

    fix: Add fix for Toll calculations
    This fixes the toll calculations not producing correct results.
    Refactor GetTollFee() to track each fee within an interval, and only update the total fee once a new interval has started or the final date processed.

--- worksamples/source/TollCalculator.cs
+++ worksamples/source/TollCalculator.cs
@@ -15,41 +15,47 @@ public class TollCalculator
         _tollFreeVehicleProvider = tollFreeVehicleProvider;
         _tollFeeProvider = tollFeeProvider;
     }
-
-    /**
-     * Calculate the total toll fee for one day
-     *
-     * @param vehicle - the vehicle
-     * @param dates   - date and time of all passes on one day
-     * @return - the total toll fee for that day
-     */
-
+    
+    /// <summary>
+    /// Calculate the total toll fee for one day
+    /// </summary>
+    /// <param name="vehicle">the vehicle</param>
+    /// <param name="dates">date and time of all passes on one day</param>
+    /// <returns>the total toll fee for that day</returns>
     public int GetTollFee(IVehicle vehicle, DateTime[] dates)
     {
         // Assuming that the all entries are within the same day, and that the entries are in order
-        DateTime intervalStart = dates[0];
+        var intervalStart = dates[0];
 
         if (_tollFreeVehicleProvider.IsTollFreeVehicle(vehicle)) return 0;
         if (_tollFreeDateProvider.IsTollFreeDate(intervalStart)) return 0;
         
-        int totalFee = 0;
-        foreach (DateTime date in dates)
-        {
-            int nextFee = _tollFeeProvider.GetTollFee(date);
-            int tempFee = _tollFeeProvider.GetTollFee(intervalStart);
+        var totalFee = 0;
+        var workingIntervalFees = new List<int>();
 
+        foreach (var date in dates)
+        {
+            if (intervalStart == date)
+            {
+                workingIntervalFees.Add(_tollFeeProvider.GetTollFee(date));
+                continue;
+            }
+            
             var diffInMinutes = (date - intervalStart).TotalMinutes;
-            if (diffInMinutes <= 60.0)
+            if (diffInMinutes > 60.0)
             {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
+                // Add the max fee of the previous interval and start a new one
+                totalFee += workingIntervalFees.Max();
+                workingIntervalFees.Clear();
+                intervalStart = date;
             }
-            else
-            {
-                totalFee += nextFee;
-            }
+ 
+            workingIntervalFees.Add(_tollFeeProvider.GetTollFee(date));
         }
+        
+        // Add the max fee of the last interval
+        totalFee += workingIntervalFees.Max();
+        
         if (totalFee > 60) totalFee = 60;
         return totalFee;
     }
